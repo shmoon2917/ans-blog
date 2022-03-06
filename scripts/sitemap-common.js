b0bd0587b3a9a8da2691330b0a052585508f2be1
Sangho Moon <shmoon2917@naver.com>
2022-03-07 02:26:30 +0900

    feat: sitemap generator 스크립트 작성

---
+++ scripts/sitemap-common.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+const globby = require('globby');
+const prettier = require('prettier');
+
+const DOMAIN = 'https://moonerd.dev';
+
+const formatted = (sitemap) => prettier.format(sitemap, { parser: 'html' });
+
+(async () => {
+  const staticPages = await globby([
+    // include
+    '../pages/**/*.tsx',
+    '../pages/*.tsx',
+    // exclude
+    '!../pages/articles/*',
+    '!../pages/_app.tsx',
+    '!../pages/_document.tsx',
+    '!../pages/_error.tsx',
+    '!../pages/404.tsx',
+    '!../pages/api/*',
+  ]);
+
+  console.log(staticPages);
+
+  const pagesSitemap = `
+    ${staticPages
+      .map((page) => {
+        const path = page
+          .replace('../pages/', '')
+          .replace('.tsx', '')
+          .replace(/\/index/g, '');
+        const routePath = path === 'index' ? '' : path;
+
+        return `
+            <url>
+                <loc>${DOMAIN}/${routePath}</loc>
+                <lastmod>${new Date().toISOString()}</lastmod>
+            </url>
+        `;
+      })
+      .join('')}
+  `;
+
+  const generatedSitemap = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">\n${pagesSitemap}</urlset>`;
+  const formattedSitemap = formatted(generatedSitemap);
+
+  fs.writeFileSync('../public/sitemap/sitemap-common.xml', formattedSitemap, 'utf-8');
+})();
