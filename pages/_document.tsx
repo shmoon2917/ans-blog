a96fe04a798d2024521c8f13e52a21bf528a22c6
Sangho Moon <shmoon2917@naver.com>
2022-03-09 02:19:29 +0900

    Revert "chore: google search console 도메인 소유권 확인을 위한 메타 태그 등록"
    This reverts commit 9de903c02887badcbdf7d29b502951c9586a868f.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -37,16 +37,11 @@ export default class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link rel="icon" href="/favicon.ico" />
-          <link
-            rel="stylesheet"
-            type="text/css"
-            href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard-dynamic-subset.css"
-          />
+          <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard-dynamic-subset.css" />
           <link
             href="https://fonts.googleapis.com/css2?family=Montserrat+Alternates:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
             rel="stylesheet"
           />
-          <meta name="google-site-verification" content="oIgwJ_U1iDOs4jg7Laab5PH6hmYMNyibNXrEwJsRrFw" />
         </Head>
         <body>
           <Main />
