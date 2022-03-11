000452a1b4ee893bcb466231784352f7c881a1ef
Sangho Moon <shmoon2917@naver.com>
2022-03-12 02:39:44 +0900

    fix: ogImage 사용 롤백

--- pages/articles/[...slug].tsx
+++ pages/articles/[...slug].tsx
@@ -72,7 +72,8 @@ export const getStaticProps: GetStaticProps<Props, ContextParams> = async ({ par
     },
   });
 
-  const ogImagePath = await generateOpenGraphImage(`/og?title=${article.title}`);
+  const ogImagePath = '';
+  // await generateOpenGraphImage(`/og?title=${article.title}`);
 
   return {
     props: {
