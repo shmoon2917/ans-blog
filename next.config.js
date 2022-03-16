a59e2d87740981019daeac52201b7693530b90dd
Sangho Moon <shmoon2917@naver.com>
2022-03-17 00:02:04 +0900

    chore: favicon 교체

--- next.config.js
+++ next.config.js
@@ -3,15 +3,6 @@ const withMDX = require('@next/mdx')({
 });
 
 module.exports = withMDX({
-  async redirects() {
-    return [
-      {
-        source: '/react/React%20hook%EC%9D%98%20lifecycle%2C%20useEffect%EB%A5%BC%20%EC%A4%91%EC%8B%AC%EC%9C%BC%EB%A1%9C',
-        destination: '/articles/react/react-hook-lifecycle',
-        permanent: true,
-      },
-    ];
-  },
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
   experimental: { workerThreads: true },
   webpack: (config, { isServer }) => {
