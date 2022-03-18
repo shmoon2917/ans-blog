01c111148e944638a2e96d374ca461c05d12bb5b
Sangho Moon <shmoon2917@naver.com>
2022-03-19 03:02:37 +0900

    refactor: ArticleImage caption 처리

--- components/Article/ArticleCodeBlock.tsx
+++ components/Article/ArticleCodeBlock.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import { SyntaxHighlighterProps, Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { dracula } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+import { cb } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
 const ArticleCodeBlock = ({ className, ...props }: SyntaxHighlighterProps): JSX.Element => {
   const match = /language-(\w+)/.exec(className || '');
-  return match ? (
-    <SyntaxHighlighter language={match[1]} PreTag="div" style={dracula} {...props} />
-  ) : (
-    <code className={className} {...props} />
-  );
+  return match ? <SyntaxHighlighter language={match[1]} PreTag="div" style={cb} {...props} /> : <code className={className} {...props} />;
 };
 
 export default ArticleCodeBlock;
--- components/Article/ArticleImage.tsx
+++ components/Article/ArticleImage.tsx
@@ -1,28 +1,53 @@
 import Image, { ImageProps } from 'next/image';
 import React, { useEffect, useState } from 'react';
+import { STYLES } from 'services/constants';
 import { useIsMounted } from 'services/hooks';
 import styled, { css } from 'styled-components';
 
-const ArticleImage = (props: ImageProps) => {
+interface Props extends ImageProps {
+  title?: string;
+}
+
+const ArticleImage = (props: Props) => {
+  const { width, height, title, alt } = props;
   const isMounted = useIsMounted();
 
-  return (
-    isMounted && (
-      <ImageWrapper style={{ maxWidth: props.width, maxHeight: props.height }}>
-        <Image {...props} alt={props.alt} layout="responsive" loading="lazy" quality={100} />
+  if (!isMounted) return null;
+
+  if (!!title) {
+    return (
+      <ImageWrapper style={{ maxWidth: width, maxHeight: height }}>
+        <Image {...props} alt={alt} loading="lazy" quality={100} />
+        <figcaption>{title}</figcaption>
       </ImageWrapper>
-    )
-  );
+    );
+  } else {
+    return (
+      <ImageWrapper as="div">
+        <Image {...props} alt={alt} layout="fill" loading="lazy" quality={100} />
+      </ImageWrapper>
+    );
+  }
 };
 
 export default ArticleImage;
 
-const ImageWrapper = styled.div`
+const ImageWrapper = styled.figure`
+  position: relative;
   display: block;
-  margin: 0 auto;
+  margin: 0 auto ${({ theme }) => theme.margin._16};
+  text-align: center;
 
   * {
     border-radius: 5px;
     transition: 0.2s;
+  }
+
+  figcaption {
+    margin-top: ${({ theme }) => theme.margin._2};
+    line-height: 1.55rem;
+    font-size: 0.85rem;
+    letter-spacing: -0.025em;
+    color: ${STYLES.color.greyscale500};
   }
 `;
